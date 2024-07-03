f4b5789257c8028e9b2b85c0ae2fdc341534822f
IM_AlirezaYT <125809189+alirezayt@users.noreply.github.com>
2024-07-03 22:20:30 +0330

    Add files via upload

---
+++ ytvision.py
@@ -0,0 +1,8 @@
+import importlib.util
+
+module_name = 'pack.cpython-311'  # نام ماژول بدون پسوند
+module_path = 'pack.cpython-311.pyc'  # مسیر فایل pyc
+
+spec = importlib.util.spec_from_file_location(module_name, module_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
